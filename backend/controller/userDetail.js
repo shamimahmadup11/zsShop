1bc6c5e34761637b08ab04dbeb0f3fcd565dfe4b
shamimahmadup11 <shamimahmadup1@gmail.com>
2024-08-22 20:29:12 +0530

    done

--- backend/controller/userDetail.js
+++ backend/controller/userDetail.js
@@ -1,52 +1,36 @@
-// const UserModel=require("../model/UserModel")
-
-// const UserDetail=async(req,res)=>{
-//     try{
-//         // const user=await UserModel.findById({_id:_id})
-//         // console.log(user)
-//         // if(!user.token){
-//         //     res.json(user)
-//         // }
-//         console.log("")
-//         }
-//         catch(e){
-//             res.json({
-//                 success:false,
-//                 message:"user not found"
-//             })
-//         }
-// }
-
-// const Details={
-//     UserDetail
-// }
-// module.exports=Details
-
 const UserModel = require("../model/UserModel");
 
 const UserDetail = async (req, res) => {
     try {
-        const userId =req.user?.id;
-        console.log(userId)
-        const user = await UserModel.findById(userId);
+        const userId = req.user?.id;
+
+        if (!userId) {
+            return res.status(400).json({
+                success: false,
+                message: "User ID is missing from request",
+            });
+        }
+
+        const user = await UserModel.findById(userId).select('-password'); // Exclude password
 
         if (!user) {
             return res.status(404).json({
                 success: false,
-                message: "User not found"
+                message: "User not found",
             });
         }
+
         res.status(200).json({
             success: true,
             message: "User details fetched successfully",
-            data: user
+            data: user,
         });
 
     } catch (e) {
+        console.error("Error fetching user details:", e.message);
         res.status(500).json({
             success: false,
             message: "An error occurred while fetching user details",
-            error: e.message
         });
     }
 };
